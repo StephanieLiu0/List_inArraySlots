bf6c8563d6cd52d2e244d72a565c77a9ae51ea0c
Stephanie Liu <sliu00@stuy.edu>
2019-03-06 22:52:38 -0500

    Updated Lists_inArraySlots.java

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -6,28 +6,38 @@
 public class List_inArraySlots {
 
     // declare fields here
+    private int[] list;
+    private int filledElements;
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
-    
+	list = new int[10];
+	filledElements = 0;
     }
 
 
     /** 
       @return the number of elements in this list
      */
-    // public int size() {
-    // }
+    public int size() {
+	return filledElements;
+    }
 
 
      /** 
        @return a string representation of this list,
        in [a,b,c,] format
       */ 
-    // public String toString() {
-    // }
+    public String toString() {
+	String output = "[";
+	for (int i = 0; i < filledElements; i++) {
+	    output += list[i] + ",";
+	}
+	output += "]";
+	return output;
+    }
 
     
     /** 
@@ -35,21 +45,24 @@ public class List_inArraySlots {
       
       @return true, in keeping with conventions yet to be discussed
      */
-     // public boolean add( int value) {
-     // }
+     public boolean add( int value) {
+	 if (filledElements == list.length)
+	     expand();
+	 list[filledElements] = value;
+	 return true;
+     }
 
 
     /** 
       Double the capacity of the List_inArraySlots, 
       preserving existing data
      */
-     // private void expand() {
-        // System.out.println( "expand... (for debugging)");
-           // /* S.O.P. rules for debugging:
-              // Working methods should be silent. But during 
-              // development, the programmer must verify that 
-              // this method is called when that is appropriate.
-              // So test using the println(), then comment it out.
-              // */
-     // }
+     private void expand() {
+	 System.out.println( "expand... (for debugging)");
+	 int[] oldList = list;
+	 list = new int[filledElements * 2];
+	 for (int i = 0; i < filledElements; i++) {
+	     list[i] = oldList[i];
+	 }
+     }
 }
